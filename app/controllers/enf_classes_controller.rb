4001f1086783ff34bce32c2ed1624cf057946219
bsutt123 <bsutt123@gmail.com>
2017-09-06 10:47:45 -0400

    added in students and instructors

--- app/controllers/enf_classes_controller.rb
+++ app/controllers/enf_classes_controller.rb
@@ -17,7 +17,10 @@ class EnfClassesController < ApplicationController
   def create
     @session = Session.find(params[:session_id])
     @enf_class = @session.enf_classes.create(enf_class_params)
-
+    session_camper_ids = params[:enf_class][:session_camper_ids]
+    session_counselor_ids = params[:enf_class][:session_counselor_ids]
+    create_instructors(session_counselor_ids, @enf_class)
+    create_students(session_camper_ids, @enf_class)
     if @enf_class.save
       flash[:notice] = "Your class was successfully saved"
       redirect_to [@session, @enf_class]
@@ -43,9 +46,16 @@ class EnfClassesController < ApplicationController
   def update
     @session = Session.find(params[:session_id])
     @enf_class = @session.enf_classes.find(params[:id])
+    session_camper_ids = params[:enf_class][:session_camper_ids]
+    session_counselor_ids = params[:enf_class][:session_counselor_ids]
 
     @enf_class.assign_attributes(enf_class_params)
 
+    detonate_students(@enf_class)
+    detonate_instructors(@enf_class)
+
+    create_instructors(session_counselor_ids, @enf_class)
+    create_students(session_camper_ids, @enf_class)
     if @enf_class.save
       flash[:notice] = "Your class was successfully saved"
     else
@@ -61,5 +71,34 @@ class EnfClassesController < ApplicationController
     params.require(:enf_class).permit(:name)
   end
 
+  def create_students(array, enf_class)
+    array.each do |s_camper_id|
+      if s_camper_id != ""
+        Student.create(session_camper: SessionCamper.find(s_camper_id), enf_class: enf_class)
+      end
+    end
+  end
+
+  def create_instructors(array, enf_class)
+    array.each do |s_counselor_id|
+      if s_counselor_id != ""
+        Instructor.create(session_counselor: SessionCounselor.find(s_counselor_i), enf_class: enf_class)
+      end
+    end
+  end
+
+  def detonate_students(enf_class)
+    students = enf_class.students
+    students.each do |student|
+      student.destroy
+    end
+  end
+
+  def detonate_instructors(enf_class)
+    instructors = enf_class.instructors
+    instructors.each do |instructor|
+      instructor.destroy
+    end
+  end
 
 end
--- app/helpers/enf_classes_helper.rb
+++ app/helpers/enf_classes_helper.rb
@@ -1,2 +1,10 @@
 module EnfClassesHelper
+  def session_camper_proc
+    return Proc.new {|x| x.camper.name}
+  end
+
+  def session_counselor_proc
+    return Proc.new {|x| x.counselor.name}
+  end
+
 end
